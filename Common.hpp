54d00c513fa6c0d4b771a63747db2a597411b410
ysrhlmky <yasarhalimkaya@gmail.com>
2017-01-03 12:28:23 +0200

    Move md5sum functionality to Common Update Runner

--- Common.hpp
+++ Common.hpp
@@ -2,10 +2,28 @@
 #define COMMON_HPP_
 
 #include <string>
+#include <sstream>
+#include <iostream>
 
 typedef struct _ServerConf {
 	std::string ipAddr;
 	int port;
 } ServerConf;
+
+inline bool md5sum(std::string filePath) {
+	std::stringstream ss;
+
+#ifdef __APPLE__
+	ss << "md5 " << filePath;
+#elif __linux__
+	ss << "md5sum " << filePath;
+#endif
+
+	if (-1 == system(ss.str().c_str())) {
+		return false;
+	}
+
+	return true;
+}
 
 #endif /* COMMON_HPP_ */
--- Runner.cpp
+++ Runner.cpp
@@ -11,14 +11,13 @@
 #include <cstdlib>
 #include <iostream>
 #include <fstream>
-#include <sstream>
 
 int main(int argc, char* argv[]) {
 
 	// Parse arguments
 	if (argc < 3) {
 		std::cout << "Specify at least two server interface addresses and ports" << std::endl;
-		return 0;
+		return -1;
 	}
 
 	std::vector<ServerConf> servers;
@@ -59,7 +58,7 @@ int main(int argc, char* argv[]) {
 	uint8_t numberOfFiles = fileListResponse.getNumberOfFiles();
 	std::cout << "Number of files : " << (int)numberOfFiles << std::endl;
 
-	for (int i = 1; i < numberOfFiles; i++) {
+	for (int i = 0; i < numberOfFiles; i++) {
 		std::cout << "File id : " << (int)fileListResponse.getFileId(i) << " File name : " << fileListResponse.getFileName(i) << std::endl;
 		std::cout << "Requesting size..." << std::endl;
 
@@ -71,35 +70,7 @@ int main(int argc, char* argv[]) {
 
 		std::cout << "Size received : " << fileSizeResponse.getFileSize()
 				<< " for file with id : " << (int)fileSizeResponse.getFileId() << std::endl;
-
-		std::cout << "Requesting some data..." << std::endl;
-		FileDataRequest fileDataRequest(fileListResponse.getFileId(i), 1, fileSizeResponse.getFileSize());
-		connection.send(fileDataRequest);
-
-		FileDataResponse fileDataResponse;
-		connection.recv(fileDataResponse);
-
-		std::cout << "Data received for file with id : " << (int)fileDataResponse.getFileId()
-				<< " startByte : " << fileDataResponse.getStartByte()
-				<< " endByte : " << fileDataResponse.getEndByte()
-				<< " dataSize : " << fileDataResponse.getDataSize() << std::endl;
-
-		std::ofstream myfile(fileListResponse.getFileName(i));
-		myfile << fileDataResponse.getData();
-		myfile.close();
-
-		std::stringstream ss;
-
-#ifdef __APPLE__
-		ss << "md5 " << fileListResponse.getFileName(i);
-#elif __linux__
-		ss << "md5sum " << fileListResponse.getFileName(i);
-#endif
-
-		system(ss.str().c_str());
 	}
-
-	//connection2.send(fileListRequest);
 
 	return 0;
 }
